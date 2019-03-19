5b5e1bdcceaedd3edd382f8e1babe81a970444cd
Lukasz Galek <lukasz.galek@o2.pl>
2019-03-19 20:28:54 +0100

    proba

---
+++ src/main/java/SDA/Book.java
@@ -0,0 +1,6 @@
+package SDA;
+
+public class Book {
+    private String title;
+    private String author;
+}
---
+++ src/main/java/SDA/Books.java
@@ -0,0 +1,10 @@
+package SDA;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Books {
+    private Book book;
+    private Map<Book,Integer> spisKsiazek = new HashMap<>();
+}
